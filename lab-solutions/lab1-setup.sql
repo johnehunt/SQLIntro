5b99624238d77713e683d601396b78b01268a7a1
John Hunt <john.hunt10@gmail.com>
2020-09-03 12:10:57 +0100

    Fixed deprecated definitions

--- lab-solutions/lab1-setup.sql
+++ lab-solutions/lab1-setup.sql
@@ -5,19 +5,19 @@ CREATE TABLE `trading`.`equity` (
   `price` DECIMAL(10,2) NULL,
   PRIMARY KEY (`id`));
 
-INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES ('1', 'IBM', 'IBM', '123.66');
-INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES ('2', 'ORCL', 'Oracle Corporation', '55.61');
-INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES ('3', 'HPQ', 'HP Inc.', '18.19');
-INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES ('4', 'INTC', 'Intel Corporation', '49.11');
-INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES ('5', 'MSFT', 'Microsoft Corporation', '212.05');
-INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES ('6', 'AAPL', 'Apple Inc.', '469.33');
+INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES (1, 'IBM', 'IBM', 123.66);
+INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES (2, 'ORCL', 'Oracle Corporation', 55.61);
+INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES (3, 'HPQ', 'HP Inc.', 18.19);
+INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES (4, 'INTC', 'Intel Corporation', 49.11);
+INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES (5, 'MSFT', 'Microsoft Corporation', 212.05);
+INSERT INTO `trading`.`equity` (`id`, `symbol`, `name`, `price`) VALUES (6, 'AAPL', 'Apple Inc.', 469.33);
 
 
 CREATE TABLE `trades` (
-  `id` int(11) NOT NULL,
-  `client_id` int(11) NOT NULL,
-  `equity_id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
+  `id` Int NOT NULL,
+  `client_id` Int NOT NULL,
+  `equity_id` Int NOT NULL,
+  `amount` Int NOT NULL,
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
