e345bfbde8bec028cfca139cd5566378151c7a81
John Hunt <john.hunt10@gmail.com>
2020-09-07 18:29:24 +0100

    Added further sub query examples

--- 07-subqueries/subqueries.sql
+++ 07-subqueries/subqueries.sql
@@ -1,3 +1,8 @@
+-- Subquery in FROM clause
+SELECT module, average - 10 AS adjusted_avg
+    FROM (SELECT module, AVG(mark) AS average FROM module_results GROUP BY module) AS tbl 
+    WHERE average > 50;
+
 -- SQL IN SELECT subquery
 SELECT * FROM modules 
          WHERE id 
@@ -24,6 +29,10 @@ SELECT name
        WHERE id = ANY(SELECT module_id 
                              FROM subject_details 
                              WHERE numbers > 9);
+-- Using a Row Subquery
+SELECT id, name, surname, mark
+    FROM results
+    WHERE ROW(name, surname) = ANY (SELECT name, surname FROM students);
 
 -- Using the Exists operator
 SELECT * FROM modules 
@@ -31,3 +40,12 @@ SELECT * FROM modules
     (SELECT name FROM subject_details 
                  WHERE modules.id = subject_details.module_id)
   ORDER BY id;
+
+-- Sub Query in a Case Statement
+SELECT id, subject, module_id, numbers,
+    CASE 
+        WHEN module_id = ANY (SELECT id FROM modules) THEN 'Registered'
+        ELSE 'NOT REGISTERED' 
+    END AS module_status
+FROM subject_details;
+
