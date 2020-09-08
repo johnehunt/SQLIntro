3cb6652203ff66ab0f19bb3ffdce3717980bea30
johnehunt <john.hunt10@gmail.com>
2020-09-08 13:31:30 +0100

    added to make it easier to set up

---
+++ lab-solutions/lab1-initial-table-data.sql
@@ -0,0 +1,10 @@
+SELECT * FROM clients;
+
+INSERT INTO clients (`id`, `name`, `surname`, `company`, `email`, `credit_limit`) VALUES (1, 'John', 'Smith', 'PYMCO', 'js@pymco', 999.99); 
+INSERT INTO `clients` (`id`, `name`, `surname`, `email`, `credit_limit`) VALUES (2, 'Eloise', 'Jones', 'ej@ggmail.com', 100.00);
+INSERT INTO `clients` (`id`, `name`, `surname`, `company`, `email`, `credit_limit`) VALUES (3, 'Adam', 'Cooke', 'MISCOM', 'ac@mis.com', 500.10); 
+INSERT INTO `clients` (`id`, `name`, `surname`, `company`, `email`, `credit_limit`) VALUES (4, 'Jasmine', 'Moore', 'stand.com', 'jm@stand.com', 750.55); 
+INSERT INTO `clients` (`id`, `name`, `surname`, `email`, `credit_limit`) VALUES ('5', 'Phoebe', 'Byrne', 'pb@ggmail.com', 900.00); 
+INSERT INTO `clients` (`id`, `name`, `surname`, `company`, `email`, `credit_limit`) VALUES (6, 'Gryff', 'Andrews', 'PYMCO', 'ga@pymco.com', 333.33); 
+ 
+ 
