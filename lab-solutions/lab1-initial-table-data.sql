aeaa146504fd7b3b45928fb09b8c8f72b0476c91
John Hunt <john.hunt10@gmail.com>
2020-09-08 16:10:58 +0100

    Example of defining table for clients and populating it with data

--- lab-solutions/lab1-initial-table-data.sql
+++ lab-solutions/lab1-initial-table-data.sql
@@ -1,4 +1,16 @@
-SELECT * FROM clients;
+USE trading;
+
+DROP TABLE IF EXISTS clients;
+
+CREATE TABLE `clients` (
+  `id` INT NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `company` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `credit_limit` DECIMAL(10,2) DEFAULT 0.0,
+  PRIMARY KEY (`id`)
+);
 
 INSERT INTO clients (`id`, `name`, `surname`, `company`, `email`, `credit_limit`) VALUES (1, 'John', 'Smith', 'PYMCO', 'js@pymco', 999.99); 
 INSERT INTO `clients` (`id`, `name`, `surname`, `email`, `credit_limit`) VALUES (2, 'Eloise', 'Jones', 'ej@ggmail.com', 100.00);
@@ -7,4 +19,6 @@ INSERT INTO `clients` (`id`, `name`, `surname`, `company`, `email`, `credit_limi
 INSERT INTO `clients` (`id`, `name`, `surname`, `email`, `credit_limit`) VALUES ('5', 'Phoebe', 'Byrne', 'pb@ggmail.com', 900.00); 
 INSERT INTO `clients` (`id`, `name`, `surname`, `company`, `email`, `credit_limit`) VALUES (6, 'Gryff', 'Andrews', 'PYMCO', 'ga@pymco.com', 333.33); 
  
+ 
+SELECT * FROM clients;
  
