a375906d6820ffea4ba1871318e7c581969fe839
johnehunt <john.hunt10@gmail.com>
2020-09-14 11:12:15 +0100

    Moved examples around

--- 06-multiple-tables/join-table-queries.sql
+++ 06-multiple-tables/join-table-queries.sql
@@ -4,7 +4,18 @@ FROM subject_details
      INNER JOIN modules 
          ON subject_details.module_id = modules.id;
 
+-- Can omit tabke from column name if not ambiguous
+SELECT subject, modules.id, name
+FROM subject_details 
+     INNER JOIN modules 
+         ON subject_details.module_id = modules.id;
 
+-- Cross Join
+SELECT subject_details.subject, modules.id, modules.name
+    FROM subject_details CROSS JOIN modules
+    WHERE modules.id = 3;
+    
+-- general select
 SELECT * from subject_details;
 
 -- Left Outer Join example
@@ -25,7 +36,3 @@ FROM subject_details
     FULL JOIN modules 
         ON subject_details.module_id = modules.id;
 
--- Cross Join
-SELECT subject_details.subject, modules.id, modules.name
-    FROM subject_details CROSS JOIN modules
-    WHERE modules.id = 3;
