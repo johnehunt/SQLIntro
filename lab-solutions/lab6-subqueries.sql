8f1723891e4b38675fc2b787784e168c5b1f4cd7
John Hunt <john.hunt10@gmail.com>
2020-09-17 14:35:24 +0100

    added further comments

--- lab-solutions/lab6-subqueries.sql
+++ lab-solutions/lab6-subqueries.sql
@@ -1,3 +1,4 @@
+-- -------------------------------------------------------------
 -- Step 1 For the trades table, find the client ids with the largest 
 -- amount for each date
 
@@ -10,14 +11,17 @@ SELECT client_id, amount, date
                MAX(amount) OVER (PARTITION BY date) as max_amount FROM trades) AS tbl
   WHERE amount = max_amount;
   
+-- -------------------------------------------------------------
 -- Step 2 Select all clients currently involved in a trade
 
 SELECT * FROM clients WHERE id IN (SELECT client_id FROM trades);
 
+-- -------------------------------------------------------------
 -- Step 3 Now find all the clients not involve din a trade
 
 SELECT * FROM clients WHERE id NOT IN (SELECT client_id FROM trades);
 
+-- -------------------------------------------------------------
 -- Step 3 Find all clients with two or more trades
 SELECT * 
     FROM clients 
