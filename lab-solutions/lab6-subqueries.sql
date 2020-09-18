87322239ebf4dff41748265fcb75bb1811fa61e3
John Hunt <john.hunt10@gmail.com>
2020-09-18 12:16:52 +0100

    Fixed condition error

--- lab-solutions/lab6-subqueries.sql
+++ lab-solutions/lab6-subqueries.sql
@@ -2,8 +2,6 @@
 -- Step 1 For the trades table, find the client ids with the largest 
 -- amount for each date
 
-SELECT client_id, date, amount FROM trades;
-
 SELECT client_id, amount, date
   FROM (SELECT client_id, 
                date, 
@@ -13,12 +11,13 @@ SELECT client_id, amount, date
   
 -- -------------------------------------------------------------
 -- Step 2 Select all clients currently involved in a trade
+
 SELECT * 
     FROM clients 
-    WHERE id IN (SELECT client_id FROM trades);
+    WHERE id IN (SELECT DISTINCT client_id FROM trades ORDER BY client_id);
 
 -- -------------------------------------------------------------
--- Step 3 Now find all the clients not involve din a trade
+-- Step 3 Now find all the clients not involved in a trade
 
 SELECT * 
     FROM clients 
@@ -28,8 +27,8 @@ SELECT *
 -- Step 4 Find all clients with two or more trades
 SELECT * 
     FROM clients 
-	WHERE id = ANY (SELECT client_id 
-				        FROM trades 
-                        GROUP BY client_id 
-                        HAVING COUNT(client_id) > 2);
- 
+	WHERE id IN (SELECT client_id 
+				    FROM trades 
+					GROUP BY client_id 
+					HAVING COUNT(client_id) >= 2);
+
